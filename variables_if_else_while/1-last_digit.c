6e7d04970e41d7a3358fc0c69e8e5022c686d377
SeratiVilankulu <seratimotla@gmail.com>
2024-03-05 10:50:59 +0000

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 v = n % 10;
 if (v > 5)
-printf("Last digit of %d is  %d and is greater than 5\n", n, v);
+printf("Last digit of %d is %d and is greater than 5\n", n, v);
 else if (v == 0)
 printf("Last digit of %d is %d and is 0\n", n, v);
 if (v < 6 && v != 0)
-printf("Last digit of %d is  %d and is greater than 5\n", n, v);
+printf("Last digit of %d is %d and is greater than 5\n", n, v);
 return (0);
 }
