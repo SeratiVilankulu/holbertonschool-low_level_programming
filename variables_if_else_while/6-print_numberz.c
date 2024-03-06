fa92086035471f9863c64d10f34d2e62184632e5
SeratiVilankulu <seratimotla@gmail.com>
2024-03-06 10:14:55 +0000

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - C program that prints numbers of base 10 from 0
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 0; t < 10; t++)
+		putchar(t);
+	putchar('\n');
+	return (0);
+}
