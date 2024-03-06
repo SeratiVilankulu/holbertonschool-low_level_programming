ae2b083fae991fcf0a14f806acbd9462b27f2823
SeratiVilankulu <seratimotla@gmail.com>
2024-03-06 10:26:52 +0000

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char l = 'z';
+
+	while
+		(l >= 'a') {
+			putchar(l);
+			l--;
+		}
+	putchar('\n');
+	return (0);
+}
