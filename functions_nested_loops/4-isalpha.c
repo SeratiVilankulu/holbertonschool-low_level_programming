100fed4b53eb51f233f052ec36beebfd34aea886
SeratiVilankulu <seratimotla@gmail.com>
2024-03-07 10:05:22 +0000

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha - Function that checks for alphabetic character
  * @c: The character to be checked
- * Return: 1 for alphabetic character c and 0 for anything else
+ * Return: 1 for alphabetic character and 0 for anything else
  */
 
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 60))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
