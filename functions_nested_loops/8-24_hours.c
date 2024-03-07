7f16c41df971072b63ebbe62fde2b0d3e3ad4aba
SeratiVilankulu <seratimotla@gmail.com>
2024-03-07 13:07:56 +0000

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int s, v, g, m;
 
-	for (s = 0; s < 2; s++)
+	for (s = 0; s <= 2; s++)
 	{
-	for (g = 0; g < 9; g++)
+	for (g = 0; g <= 9; g++)
 	{
 		if ((s <= 1 && g <= 9) || (s <= 2 && g <= 3))
 		{
