400d0e3e9322f9bc690c70c6f513605b123b37eb
SeratiVilankulu <seratimotla@gmail.com>
2024-03-11 08:20:35 +0000

    I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers - Function that prints the numbers, from 0 to 9
@@ -9,15 +10,15 @@ void print_most_numbers(void)
 {
 	int g;
 
-	for (g = 48; g <= 58; g++)
+	for (g = 48; g <= 57; g++)
 	{
 		if (g != 50)
 		{
 			if (g != 52)
-			}
+			{
 				_putchar(g);
 			}
 		}
-	}
 	_putchar('\n');
+	}
 }
