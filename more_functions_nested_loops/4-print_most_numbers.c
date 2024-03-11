31bb136c007d78dca7421b7c583000983a7d95aa
SeratiVilankulu <seratimotla@gmail.com>
2024-03-11 08:03:38 +0000

    I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,13 @@ void print_most_numbers(void)
 
 	for (g = 48; g <= 58; g++)
 	{
-		if (g != 50 || g != 52)
-			_putchar(g);
+		if (g != 50)
+		{
+			if (g != 52)
+			}
+				_putchar(g);
+			}
+		}
 	}
 	_putchar('\n');
 }
