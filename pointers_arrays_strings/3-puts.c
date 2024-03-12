09fbc0f31eece8f4fb33d2bf25c650c5075fdb06
SeratiVilankulu <seratimotla@gmail.com>
2024-03-12 11:01:04 +0000

    I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string
+ * @str: Function parameter
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+		_putchar('\n');
+}
