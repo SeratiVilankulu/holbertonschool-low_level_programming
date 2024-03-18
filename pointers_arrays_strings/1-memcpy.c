b860f8a0835ddfb4cca1a2b6286575f03ed25cc0
SeratiVilankulu <seratimotla@gmail.com>
2024-03-18 06:20:37 +0000

    memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Function that copies memory area
+ * @dest: The address of memory to print
+ * @src: The value to be copied
+ * @n: The size of the memory to print
+ * Return: 
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int h = 0;
+	int v = n;
+
+		for (;h < v; h++)
+		{
+			dest[r] = src[r];
+			n--;
+		}
+		return (dest);
+}
