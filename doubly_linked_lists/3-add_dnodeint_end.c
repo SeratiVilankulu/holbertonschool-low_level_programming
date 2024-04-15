540fd6b9baf2d462c7ac7e25df60f7660571852b
SeratiVilankulu <seratimotla@gmail.com>
2024-04-15 09:31:29 +0000

    Insert at index

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,41 +1,37 @@
-#include "lists.h"
-#include <stddef.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
- *
- * @head: head of the list
- * @n: value of the element
- * Return: the address of the new element
- */
+* add_dnodeint_end - Adds a new node at the end
+* of a doubly linked list
+* @head: The head of the doubly linked list
+* @n: The number to place in the new node
+*
+* Return: The new head of the doubly linked list
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *h;
-	dlistint_t *new;
+dlistint_t *current = NULL, *new_node = NULL;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-	new->n = n;
-	new->next = NULL;
+new_node->n = n;
+if (*head)
+{
+current = *head;
+while (current->next != NULL)
+current = current->next;
 
-	h = *head;
+new_node->next = NULL;
+new_node->prev = current;
+current->next = new_node;
+return (new_node);
+}
 
-	if (h != NULL)
-	{
-		while (h->next != NULL)
-			h = h->next;
-		h->next = new;
-	}
-	else
-	{
-		*head = new;
-	}
-
-	new->prev = h;
-
-	return (new);
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
+return (*head);
 }
